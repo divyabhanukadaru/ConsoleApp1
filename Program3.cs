dcae369f5f246930c8d0530a478282814e77fbe6
vinod <gattuvinodkumar232@gmail.com>
2025-04-29 19:47:48 +1000

    added new addition feature

--- Program3.cs
+++ Program3.cs
@@ -4,23 +4,20 @@ namespace ConsoleApp1;
 
 public class Program3
 {
+    public static int SumOfThreeNumbers(int a, int b, int c)
+    {
+        return a + b + c;
+    }
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter a number:");
-        if (int.TryParse(Console.ReadLine(), out int number))
-        {
-            if (number % 2 == 0)
-            {
-                Console.WriteLine($"{number} is even.");
-            }
-            else
-            {
-                Console.WriteLine($"{number} is odd.");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Invalid input. Please enter a valid integer.");
-        }
+        Console.WriteLine("Enter three numbers to sum:");
+        Console.Write("First number: ");
+        int firstNumber = int.Parse(Console.ReadLine() ?? "0");
+        Console.Write("Second number: ");
+        int secondNumber = int.Parse(Console.ReadLine() ?? "0");
+        Console.Write("Third number: ");
+        int thirdNumber = int.Parse(Console.ReadLine() ?? "0");
+        int result = SumOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
+        Console.WriteLine($"The sum of {firstNumber}, {secondNumber}, and {thirdNumber} is: {SumOfThreeNumbers(firstNumber, secondNumber, thirdNumber)}");
     }
 }
